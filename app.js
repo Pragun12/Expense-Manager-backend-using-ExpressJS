20a2285fae92b91e95d39c3fcb7e443d399f77aa
Pragun <pragse_p@live.com>
2018-07-20 10:23:33 -0500

    Rest API

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ var bodyParser=require('body-parser');
 var path=require('path');
 var cors = require('cors');
 var mongojs = require('mongojs');
-var db = mongojs('expenseapp', ['users']);
+var multer=require('multer');
+var db = mongojs('expenseapp', ['users','expenses']);
 var app=express();
+app.use(express.static('public'))
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+var upload = multer({ storage:storage });
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cors());
 
-var users=[
-    {
-        id:1,
-        firstname:"Pragun",
-        lastname:"Pradhan",
-        email:"pragse_p@live.com",
-        password:"Pragun12"
-    }
-]
+
 
 
     app.get('/',function(req,res){
-        res.json(users);
+        res.send('Hello World');
         });
+
+        app.get('/api/expenses',function(req,res){
+            
+            db.expenses.find({userid:req.query.userid}, function(err, doc) {
+                res.json(doc);
+             })
+            
+            });
 
 
         app.post('/api/auth',function(req,res){
@@ -56,6 +69,33 @@ var users=[
     });
     
     //res.json(users[0]);
+     });
+
+
+
+     app.post('/api/expense',upload.single('file'),function(req,res,next){     
+         
+        
+
+      var expenseInfo={
+            userid:req.body.userid,
+            merchant:req.body.merchant,
+            date:req.body.date,
+            total:req.body.total,
+            category:req.body.category,
+            comment:req.body.comment,
+            file:req.file.originalname
+
+            
+       };
+
+
+       db.expenses.insert(expenseInfo, function(err,result){
+        res.json(result);
+
+    });
+    
+   
      });
 
     app.listen(3200,function(){
